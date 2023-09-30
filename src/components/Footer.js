d3d63c789abf105ad2a4b95c34b4094caf973f4a
darshan1472 <87933427+darshan1472@users.noreply.github.com>
2023-09-30 16:00:02 +0530

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,67 +1,22 @@
-/* eslint-disable react/jsx-no-undef */
+import React from 'react'
 
-import React, { useState } from 'react'
-import { Link, useNavigate } from "react-router-dom";
-import Badge from "@material-ui/core/Badge";
-import ShoppingCartIcon from "@material-ui/icons/ShoppingCart";
-import { useCart } from './ContextReducer';
-import Modal from '../Modal';
-import Cart from '../screens/Cart';
-export default function Navbar(props) {
-
-    const [cartView, setCartView] = useState(false)
-    localStorage.setItem('temp', "first")
-    let navigate = useNavigate();
-    const handleLogout = () => {
-        localStorage.removeItem('token')
-
-        navigate("/login")
-    }
-
-    const loadCart = () => {
-        setCartView(true)
-    }
-
-    const items = useCart();
-    return (
-        <div>
-            <nav className="navbar navbar-expand-lg navbar-dark bg-success position-sticky"
-                style={{ boxShadow: "0px 10px 20px black", filter: 'blur(20)', position: "fixed", zIndex: "10", width: "100%" }}>
-                <div className="container-fluid">
-                    <Link className="navbar-brand fs-1 fst-italic" to="/">GoFood</Link>
-                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                        <span className="navbar-toggler-icon"></span>
-                    </button>
-                    <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                        <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-                            <li className="nav-item">
-                                <Link className="nav-link fs-5 mx-3 active" aria-current="page" to="/">Home</Link>  {/* index.css - nav-link color white */}
-                            </li>
-                            {(localStorage.getItem("token")) ?
-                                <li className="nav-item">
-                                    <Link className="nav-link fs-5 mx-3 active" aria-current="page" to="/myorder" >My Orders</Link>  {/* index.css - nav-link color white */}
-                                </li> : ""}
-                        </ul>
-                        {(!localStorage.getItem("token")) ?
-                            <form className="d-flex">
-                                <Link className="btn bg-white text-success mx-1 " to="/login">Login</Link>
-                                <Link className="btn bg-white text-success mx-1" to="/signup">Signup</Link>
-                            </form> :
-                            <div>
-
-                                <div className="btn bg-white text-success mx-2 " onClick={loadCart}>
-                                    <Badge color="secondary" badgeContent={items.length} >
-                                        <ShoppingCartIcon />
-                                    </Badge>
-                                    Cart
-                                </div>
-
-                                {cartView ? <Modal onClose={() => setCartView(false)}><Cart></Cart></Modal> : ""}
-
-                                <button onClick={handleLogout} className="btn bg-white text-success" >Logout</button></div>}
-                    </div>
-                </div>
-            </nav>
-        </div>
-    )
+export default function Footer() {
+  return (
+    <div><div className="container">
+    <footer className="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top">
+      <div className="col-md-4 d-flex align-items-center">
+        <a href="/" className="mb-3 me-2 mb-md-0 text-muted text-decoration-none lh-1">
+        </a>
+        <span className="text-muted">© 2022 <i>GoFood</i>, Inc</span>
+      </div>
+  
+      <ul className="nav col-md-4 justify-content-end list-unstyled d-flex">
+        <li className="ms-3"><a className="text-muted" href="/"><svg className="bi" width="24" height="24"><use ></use></svg></a></li>
+        <li className="ms-3"><a className="text-muted" href="/"><svg className="bi" width="24" height="24"><use ></use></svg></a></li>
+        <li className="ms-3"><a className="text-muted" href="/"><svg className="bi" width="24" height="24"><use></use></svg></a></li>
+      </ul>
+    </footer>
+  </div>
+  </div>
+  )
 }
